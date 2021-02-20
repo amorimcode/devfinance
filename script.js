80efb61c175415c80e96c265127709635dd460a1
Bruno <bruno.amorim032@gmail.com>
2021-02-19 23:12:03 -0300

    auto tr's script

--- script.js
+++ script.js
@@ -54,18 +54,31 @@ const Transaction = {
 
 const DOM = {
 
-    innerHTMLTransaction() {
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
-        const html = `
-        <tr>
-            <td class='description'>Luz</td>
-            <td class='expense'>- R$ 500,00</td>
-            <td class='date'>23/01/2021</td>
-            <td>
-                <img src="./assets/minus.svg" alt="Remover Transação">
-            </td>
-    </tr>
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+
+
+    },
+
+    innerHTMLTransaction(transaction) {
+        const html = `        
+        <td class='description'>${transaction.description}</td>
+        <td class='expense'>${transaction.amount}</td>
+        <td class='date'>${transaction.date}</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remover Transação">
+        </td>
         `
-
+        return html
     }
 }
+
+// forEach = para cada elemento do array ele ira executar uma function
+transactions.forEach(function (transaction) {
+    DOM.addTransaction(transaction)
+})
